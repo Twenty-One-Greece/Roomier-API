bda7de3a4ff3400a461dbb446722984ff3f4ebac
elissaioskon <elissaioskon@gmail.com>
2018-04-11 14:56:16 +0300

    some comments to general availability tests

--- external/GeneralAvailability.js
+++ external/GeneralAvailability.js
@@ -70,7 +70,9 @@ router.post('/general-availability/', (req, res) => {
         else return handleResult(res, rows, dayDiff, data)
     })
 })
-
+// select roomTypeImages.image, roomTypes.id as roomTypeId from roomTypeImages
+//     // join roomTypes on roomTypes.id = roomTypeImages.roomTypeID
+//     // where maxAdults >= ? and maxChildren >= ? and p.city = ? and r.maxPax >= ?;
 //Elissaios test route
 //General availability Route backup
 router.post('/general-availability/test', (req, res) => {
@@ -93,9 +95,7 @@ router.post('/general-availability/test', (req, res) => {
     on r.propertyID = p.id where maxAdults >= ? 
     and maxChildren >= ? and p.city = ? and r.maxPax >= ?;
 
-    select roomTypeImages.image, roomTypes.id as roomTypeId from roomTypeImages
-    join roomTypes on roomTypes.id = roomTypeImages.roomTypeID
-    where maxAdults >= ? and maxChildren >= ? and p.city = ? and r.maxPax >= ?;
+    // 
 
     select * from rates where ((startDate between date(?) and date(?))
     or (endDate between date(?) and date(?))
