279927a2b6f25ac5ba3a713472925eb62e206dfb
elissaioskon <elissaioskon@gmail.com>
2018-04-20 16:30:33 +0300

    remove meal plan as property at reservation route in format reservation

--- external/Reservation.js
+++ external/Reservation.js
@@ -19,30 +19,30 @@ router.post('/reservation/', (req, res) => {
 
   // Data created according to data provided
   const reservation = {
-      bookingID:                           bookingID,
-      checkIn:                             data.start_date,
-      checkOut:                            data.end_date,
-      bookingOrigin:                       (data.bookingOrigin) ? data.bookingOrigin : "Onetourismo",
-      propertyID:                          data.hotelID,
-      roomTypeID:                          data.roomTypeID,
-      bookingTotalPrice:                   data.bookingPrice,
-      bookingStatus:                       "Confirmed",
-      paymentMethod:                       (data.paymentMethod) ? data.paymentMethod : "Not Defined",
-      name:                                data.leadCustomer.firstName,
-      surname:                             data.leadCustomer.lastName,
-      email:                               data.leadCustomer.email,
-      zip:                                 data.leadCustomer.postalCode,
-      address:                             data.leadCustomer.address,
-      phone:                               data.leadCustomer.telephone,
-      comments:                            data.remarks.clientRemarks,
-      mealPlan:                            data.mealPlan
+    bookingID: bookingID,
+    checkIn: data.start_date,
+    checkOut: data.end_date,
+    bookingOrigin: (data.bookingOrigin) ? data.bookingOrigin : "Onetourismo",
+    propertyID: data.hotelID,
+    roomTypeID: data.roomTypeID,
+    bookingTotalPrice: data.bookingPrice,
+    bookingStatus: "Confirmed",
+    paymentMethod: (data.paymentMethod) ? data.paymentMethod : "Not Defined",
+    name: data.leadCustomer.firstName,
+    surname: data.leadCustomer.lastName,
+    email: data.leadCustomer.email,
+    zip: data.leadCustomer.postalCode,
+    address: data.leadCustomer.address,
+    phone: data.leadCustomer.telephone,
+    comments: data.remarks.clientRemarks,
+    // mealPlan:                            data.mealPlan
   }
 
   // Make the query. Save reservation in db
   connection.query(sql, [data.roomTypeID, reservation], (err, rows) => {
 
-    if(err) console.log(err);
-    if (err) return res.send({status: 'An error ocured'})
+    if (err) console.log(err);
+    if (err) return res.send({ status: 'An error ocured' })
     else return res.send(formatReservation(data, bookingID, rows[0][0]))
   })
 })
@@ -50,21 +50,21 @@ router.post('/reservation/', (req, res) => {
 // Create object to send as responce
 formatReservation = (data, bookingID, name) => {
   return {
-    checkOut:                             data.start_date,
-    checkIn:                              data.end_date,
-    propviderStatus:                      'ACTIVE',
-    roomType:                             name,
+    checkOut: data.start_date,
+    checkIn: data.end_date,
+    propviderStatus: 'ACTIVE',
+    roomType: name,
     provider: {
-      name:                               'ROOMIER',
-      code:                               bookingID
+      name: 'ROOMIER',
+      code: bookingID
     },
-    leadCustomer:{
-      firstName:                          data.leadCustomer.firstName,
-      lastName:                           data.leadCustomer.lastName,
-      email:                              data.leadCustomer.email,
-      telephone:                          data.leadCustomer.telephone
+    leadCustomer: {
+      firstName: data.leadCustomer.firstName,
+      lastName: data.leadCustomer.lastName,
+      email: data.leadCustomer.email,
+      telephone: data.leadCustomer.telephone
     },
-    fullPrice:                            data.bookingPrice
+    fullPrice: data.bookingPrice
   }
 }
 
